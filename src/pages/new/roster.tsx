d6d32844e0aacae0688e022c76108054d66b8429
German Solano <kontrolg1239@gmail.com>
2025-01-14 12:06:13 -0400

    avances

--- src/pages/new/roster.tsx
+++ src/pages/new/roster.tsx
@@ -1,88 +1,168 @@
+import {
+  AnimatePresence,
+  motion,
+  MotionValue,
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+} from "motion/react";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
+import { useIsClient } from "usehooks-ts";
+
+const roster = [
+  {
+    fullName: "Mari Gutierrez",
+    title: "Holistic health coach-personal trainer",
+    bio: `Mari Gutiérrez es una health coach apasionada por un estilo de vida positivo y saludable.  Comparte rutinas de ejercicio accesibles y recetas saludables, siempre enfocada en mejorar la calidad de vida de su comunidad. Mari busca generar un impacto positivo promoviendo cambios simples en los hábitos diarios. 
+    `,
+    tags: ["Coach-personal trainer", "Holistic health"],
+    videoUrl: "https://examples.motion.dev/photos/cityscape/1.jpg",
+    imageUrl: "https://examples.motion.dev/photos/cityscape/1.jpg",
+    keyword: "Healt",
+  },
+  {
+    fullName: "Mari Gutierrez",
+    title: "Holistic health coach-personal trainer",
+    bio: `Mari Gutiérrez es una health coach apasionada por un estilo de vida positivo y saludable.  Comparte rutinas de ejercicio accesibles y recetas saludables, siempre enfocada en mejorar la calidad de vida de su comunidad. Mari busca generar un impacto positivo promoviendo cambios simples en los hábitos diarios. 
+    `,
+    tags: ["Coach-personal trainer", "Holistic health"],
+    videoUrl: "https://examples.motion.dev/photos/cityscape/2.jpg",
+    imageUrl: "https://examples.motion.dev/photos/cityscape/2.jpg",
+    keyword: "Health",
+  },
+];
+
+type Roster = (typeof roster)[number];
+
 export default function Roster() {
+  const isClient = useIsClient();
+  const [snappedItem, setSnappedItem] = useState(roster[0]);
+
+  useEffect(() => {
+    document.documentElement.classList.add("snap-y");
+    document.documentElement.classList.add("snap-mandatory");
+
+    return () => {
+      document.documentElement.classList.remove("snap-y");
+      document.documentElement.classList.remove("snap-mandatory");
+    };
+  }, []);
+
+  if (!isClient) return;
+
   return (
-    <div className="bg-[#AC4A86] h-full text-black font-bold">
-      <header className=" pt-4 flex justify-between text-2xl place-items-center pb-16 max-w-[1734px] px-8  mx-auto ">
-        <div className=" ">
+    <>
+      {roster.map((roster) => (
+        <RosterCard
+          key={roster.fullName}
+          roster={roster}
+          onSnapChange={(isSnapped) => isSnapped && setSnappedItem(roster)}
+        />
+      ))}
+      <div className="flex place-items-center fixed inset-y-0 right-0 text-black font-bold pr-8">
+        <span
+          className="px-8 text-[5.125rem] rounded-full border border-current bg-[#9C76E3] italic mt-16 -mr-32 relative z-30"
+          key={snappedItem.keyword}
+        >
+          {snappedItem.keyword}
+        </span>
+        <Image
+          src={require("public/assets/images/new/roster/mari-gutierrez-image.png")}
+          alt={snappedItem.fullName}
+          className="w-[437.42px] -mr-44 relative z-20"
+        />
+        <div className="relative">
           <Image
-            src={"/assets/images/fm-logo.png"}
-            alt="/"
-            height={48}
-            width={144}
-            className=""
+            src={require("public/assets/images/phone-mockup.png")}
+            className="w-[24.5644rem] z-10 relative"
+            alt="Mockup Teléfono"
           />
+          <AnimatePresence>
+            <motion.div
+              className="absolute left-6 top-4 right-[1.375rem] bottom-[1.125rem] rounded-[2rem] overflow-hidden"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              key={snappedItem.imageUrl}
+            >
+              <img
+                src={snappedItem.imageUrl}
+                alt=""
+                className="w-full h-full object-cover"
+              />
+            </motion.div>
+          </AnimatePresence>
         </div>
-        <div className="flex gap-4 justify-center ml-24 -mt-6 ">
-          <Image
-            src={"/assets/images/navbar-vector.png"}
-            width={482}
-            height={171}
-            alt="vector-navbar"
-            className="absolute -mt-10  "
-          />
-          <Link href={"/"} className="z-10">
-            nuestra casa
-          </Link>
-          <Link href={"/"} className="z-10">
-            nuestra familia
-          </Link>
-        </div>
-        <div className="flex gap-8">
-          <Link href={"/"} className="">
-            <Image
-              src={"/assets/images/hablemos-button.png"}
-              alt="/"
-              width={170}
-              height={106}
-              className=""
-            />
-          </Link>
-          <div className="flex gap-2">
-            <button>en</button>
-            <button>es</button>
-          </div>
-        </div>
-      </header>
-      <div className="flex mx-auto justify-between items-center -mt-16 max-w-[1734px] px-8">
-        <div className="flex flex-col  gap-4 leading-tight ">
-          <h1 className="text-[5rem] italic font-extrabold">MARI GUTIERREZ</h1>
-          <h2 className="text-[2.75rem] italic ">
-            Holistic health coach-personal trainer
-          </h2>
-          <div className="flex items-center gap-7 pl-2 font-extrabold mt-8  ">
-            <Image
-              src={"/assets/images/new/roster/ellipse.png"}
-              alt="/"
-              width={184 / 2}
-              height={184 / 2}
-              className=""
-            />
-            <Image
-              src={"/assets/images/new/roster/instagram-icon.png"}
-              alt="/"
-              width={108 / 2}
-              height={124 / 2}
-              className="absolute ml-[18px]"
-            />
-            <p className="text-[5.1rem] italic">373 K</p>
-          </div>
-          <div className="flex items-center gap-7 pl-2 font-extrabold ">
-            <Image
-              src={"/assets/images/new/roster/ellipse.png"}
-              alt="/"
-              width={184 / 2}
-              height={184 / 2}
-              className=""
-            />
-            <Image
-              src={"/assets/images/new/roster/tiktok-icon.png"}
-              alt="/"
-              width={88 / 2}
-              height={100 / 2}
-              className="absolute ml-6"
-            />
-            <p className="text-[5.1rem] italic">404 K</p>
+        {/* <div className="w-[768px] h-[898px]">
+          <AnimatePresence>
+            <motion.div
+              key={snappedItem.imageUrl}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className="absolute inset-0"
+            >
+              <img
+                src={snappedItem.imageUrl}
+                alt=""
+                width={1536 / 2}
+                height={1796 / 2}
+                className="absolute inset-0"
+              />
+            </motion.div>
+          </AnimatePresence>
+        </div> */}
+      </div>
+    </>
+  );
+}
+
+function useParallax(value: MotionValue<number>, distance: number) {
+  return useTransform(value, [0, 1], [-distance, distance]);
+}
+
+function RosterCard({
+  roster,
+  onSnapChange,
+}: {
+  roster: Roster;
+  onSnapChange: (isSnapped: boolean) => void;
+}) {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref });
+  const y = useParallax(scrollYProgress, 150);
+  const opacity = useTransform(scrollYProgress, [0, 0.5, 1], [0.2, 1, 0.2]);
+  const isSnapped = useTransform(
+    scrollYProgress,
+    [0, 0.25, 0.5, 0.75, 1],
+    [false, true, true, true, false]
+  );
+
+  useMotionValueEvent(isSnapped, "change", onSnapChange);
+
+  return (
+    <div className="bg-[#AC4A86] text-black font-bold h-screen snap-center [perspective:_500px] flex flex-col justify-center">
+      <div className="flex items-center max-w-[1734px] px-8" ref={ref}>
+        <motion.div
+          className="flex flex-col gap-4 leading-tight"
+          style={{ y, opacity }}
+        >
+          <h1 className="text-[5rem] italic font-extrabold uppercase">
+            {roster.fullName}
+          </h1>
+          <h2 className="text-[2.75rem] italic ">{roster.title}</h2>
+          <p className="text-2xl max-w-[43.125rem]">{roster.bio}</p>
+          <div className="flex gap-4">
+            {roster.tags.map((tag) => (
+              <span
+                key={tag}
+                className="p-4 text-2xl rounded-full border border-current"
+              >
+                {tag}
+              </span>
+            ))}
           </div>
           <div className="max-w-max flex text-center mt-4">
             <Link href={"/"}>
@@ -96,23 +176,7 @@ export default function Roster() {
               <p className="text-[1.6rem] -mt-16 -ml-2">ver más</p>
             </Link>
           </div>
-        </div>
-        <div className="flex place-items-center">
-          <Image
-            src={"/assets/images/new/roster/nube-healt.png"}
-            alt="/"
-            width={640 / 2}
-            height={336 / 2}
-            className=" absolute -ml-56 mt-32 "
-          />
-          <Image
-            src={"/assets/images/new/roster/roster-pictures.png"}
-            alt="/"
-            width={1536 / 2}
-            height={1796 / 2}
-            className=" "
-          />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
