f97c2eb8f8fb94f2c90d2563f596fdb5a2394404
Francisco Salazar (Franvierdev) <xxaverii@gmail.com>
2025-01-17 12:45:07 -0400

    form

--- src/pages/new/form.tsx
+++ src/pages/new/form.tsx
@@ -1,6 +1,19 @@
 import Header from "./header";
 import Image from "next/image";
+import { useForm } from "react-hook-form";
 export default function Form() {
+  interface formValues {
+    companyName: string;
+    contactName: string;
+    email: string;
+    contactVia: string;
+    presupuesto: string;
+    details: string;
+  }
+  const { register, handleSubmit } = useForm<formValues>();
+  const onSubmit = (data: formValues) => {
+    console.log(data);
+  };
   return (
     <div className="bg-[#EFDEC6] text-black font-bold">
       <Header />
@@ -13,56 +26,69 @@ export default function Form() {
           className="absolute -mt-20 "
         />
         <div className=" bg-[#EFDEC6] text-[#EFDEC6] h-screen  max-w-3xl mx-auto   ">
-          <form className="flex flex-col items-center relative ">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col items-center relative "
+          >
             <div className="absolute mt-32 w-full flex flex-col ">
               <h1 className="font-bold text-5xl  italic max-w-xs self-center text-center leading-tight">
                 ¡TRABAJEMOS JUNTOS!
               </h1>
               <div className="border-b flex flex-col text-2xl mt-10 ">
-                <label htmlFor="empresa">Empresa</label>
+                <label htmlFor="companyName">companyName</label>
                 <input
                   className=" bg-transparent focus:outline-none  "
                   type="text"
-                  id="empresa"
+                  {...register("companyName", { required: true })}
+                  id="companyName"
                 />
               </div>
               <div className="border-b flex flex-col text-2xl mt-10 ">
-                <label htmlFor="correo">E-mail (empresarial)</label>
-                <input
-                  className=" bg-transparent focus:outline-none  "
-                  type="email"
-                  id="correo"
-                />
-              </div>
-              <div className="border-b flex flex-col text-2xl mt-10 ">
-                <label htmlFor="tipo">Tipo de campaña o colaboración</label>
+                <label htmlFor="contactName">Nombre de contacto</label>
                 <input
                   className=" bg-transparent focus:outline-none  "
                   type="text"
-                  id="tipo"
+                  {...register("contactName", { required: true })}
+                  id="contactName"
                 />
               </div>
               <div className="border-b flex flex-col text-2xl mt-10 ">
-                <label htmlFor="presupuesto">Presupuesto estimado</label>
+                <label htmlFor="email">E-mail (companyNamerial)</label>
                 <input
                   className=" bg-transparent focus:outline-none  "
                   type="text"
-                  id="presupuesto"
+                  {...register("email", { required: true })}
+                  id="email"
                 />
               </div>
               <div className="border-b flex flex-col text-2xl mt-10 ">
-                <label htmlFor="detalles">
-                  Detalles adicionales (fecha, duración, plataformas).
+                <label htmlFor="contactVia">
+                  ¿Cómo te encontramos en la web o en redes sociales?
                 </label>
                 <input
                   className=" bg-transparent focus:outline-none  "
                   type="text"
-                  id="detalles"
+                  {...register("contactVia", { required: true })}
+                  id="contactVia"
+                />
+              </div>
+
+              <div className="border-b flex flex-col text-2xl mt-10 ">
+                <label className="flex gap-2" htmlFor="details">
+                  Detalles adicionales (fecha, duración, plataformas)
+                  <p className="font-thin">opcional</p>
+                </label>
+                <input
+                  className=" bg-transparent focus:outline-none  "
+                  type="text"
+                  {...register("details")}
+                  id="details"
                 />
                 <button className="self-end absolute -mt-28 -mr-[460px]">
                   <Image
                     src={"/assets/images/new/enviar-button.png"}
                     alt="/"
+                    typeof="submit"
                     width={586 / 2}
                     height={366 / 2}
                     className="   "
